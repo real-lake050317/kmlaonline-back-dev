9e550b0cd542c0dab3b737c149d08a8fc2ae958a
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-08-13 03:16:40 +0900

    Fixed Models

--- models/Post.js
+++ models/Post.js
@@ -24,6 +24,10 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  uploaderSchoolId: {
+    type: Number,
+    required: true
+  },
   comments: {
     type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
   },
